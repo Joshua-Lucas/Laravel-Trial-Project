66297e5373b3aecc4074b5a7c5aa28bbcc9542a4
Joshua Lucas <joshuamlucas91@gmail.com>
2019-12-14 11:14:03 -0500

    allowed for all auth users to view projects and allowed auth user to view projects from their loggin page.

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -7,8 +7,9 @@
    
     <div>
         <ul>
-            <li><a href="/projects">home</a></li>
-            <li><a href="/projects/create">new project log</a></li>
+            <li><a href="/home">home</a></li>
+            <li><a href="/create">new project log</a></li>
+            <li><a href="/projects">Work Logs</a></li>
         
         </ul>
 
