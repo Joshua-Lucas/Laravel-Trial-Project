47c8c5546661a39b81bf05c3a47fefb0126a9265
Joshua Lucas <joshuamlucas91@gmail.com>
2020-01-11 22:44:34 -0500

    complete the layout of the projects worklog page

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -3,23 +3,29 @@
 @section('title', 'Welcome')
 
 @section('content')
+<div class="flex flex-col justify-center mt-4 mx-2 lg:w-3/4 xl:w-4/5 lg:justify-start lg:shadow-xl lg:mx-0">
 
-    <h1>Project Managment Homepage</h1>
+    <div class=" mb-4 flex  flex-col items-center ">
+        <h1 class="text-5xl text-center text-forest uppercase">Company Worklogs</h1>
+    </div>
 
-    <p>This is the home page where you will see all projects being worked on. <br> If you need to add a new project click the new project log. If you want to review an existing project click the project.</p>
-  
-    <h2>Projects</h2>
+    <div class="text-center mt-2 mb-4">
+        <p>This is the homepage where you will see all projects being worked on thorughout the company.</p>
+    </div>
+    
+    <div class=" bg-forest flex flex-col flex-wrap text-center  text-linen rounded-lg shadow-xl md:mx-6 lg:w-5/6 lg:px-3 lg:self-center">
+        <h2 class="mt-3 mx-1 text-2xl font-extrabold uppercase ">Projects</h2>
+        <div class=" flex flex-col flex-wrap mt-2 mb-3 sm:flex-row sm:justify-center" >
+        @foreach($project as $project)
 
-    @foreach($project as $project)
 
-        <div>
-            <li>
-                <a href="/projects/{{ $project->id }}">
-                    {{$project->title}}
-                </a>
-            </li>
+                    <a href="/projects/{{ $project->id }}" class="m-2 p-4 bg-storm items-center text-center rounded-lg shadow-xl">
+                        {{$project->title}}
+                    </a>
+
+
+        @endforeach
         </div>
-
-    @endforeach
-
+    </div>
+</div>
 @endsection
